8c9ce5ab6059f49b6712a444404aac64ce4c1d5c
mkilic91 <m_kilic@outlook.com.tr>
2019-12-28 21:00:44 +0300

    add example user model and fixed main.go import order

---
+++ App/Models/user.go
@@ -0,0 +1,23 @@
+package Models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/goKLC/goKLC"
+	"time"
+)
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Name      string    `json:"name" gorm:"type:varchar(100)"`
+	Surname   string    `json:"surname" gorm:"type:varchar(100)"`
+	Username  string    `json:"username" gorm:"type:varchar(100);not null"`
+	Email     string    `json:"email" gorm:"type:varchar(100);unique_index;not null"`
+	Password  string    `json:"password" gorm:"type:varchar(255);not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+func init() {
+	goKLC.GetApp().DB().AutoMigrate(&User{})
+}
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
+// spaces for import priority order
 import (
-	_ "github.com/goKLC/framework/App/Middleware"
-	_ "github.com/goKLC/framework/App/Providers"
 	_ "github.com/goKLC/framework/Config"
+
+	_ "github.com/goKLC/framework/App/Providers"
+
+	_ "github.com/goKLC/framework/App/Models"
+
+	_ "github.com/goKLC/framework/App/Middleware"
+
 	_ "github.com/goKLC/framework/Route"
+
 	"github.com/goKLC/goKLC"
 )
 
